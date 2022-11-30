764514b1e80430732342cb92b8e4cdcdc283eb12
prince-konwea <kokodecontee123@gmail.com>
2022-11-30 21:40:46 +0100

    created folder architure for project and initializing redux tool kit for state management

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,41 @@
+import { useEffect } from "react";
+import Home from "./scenes/home/Home";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import CheckOut from "./scenes/checkout/CheckOut";
+import Confirmation from "./scenes/checkout/Confirmation";
+import NavBar from "./scenes/global/NavBar";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+      <NavBar />
+       <ScrollToTop />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="item/:itemId" element={<ItemDetails />} />
+        <Route path="checkout" element={<CheckOut />} />
+        <Route path="checkout/success" element={<Confirmation />} />
+
+       </Routes>
+      </BrowserRouter>
      
     </div>
   );
---
+++ client/src/state/index.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isCartOpen: false,
+    cart:[],
+    items:[],
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+})
