3c7fea89a55583963a1e571f491b25eb02ed76b4
prince-konwea <kokodecontee123@gmail.com>
2022-12-02 06:52:19 +0100

    complete carousel for home component

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,5 +1,5 @@
-import { Box, Typograhy, IconButton, useMediaQuery } from "@mui/material";
-import Carousel from "react-responsive-carousel";
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
@@ -17,7 +17,7 @@ export const heroTextureImports = importAll(
 )
 
 const MainCarousel = () => {
-  const isNOnMObile = useMediaQuery("(min-width:600px)")
+  const isNonMobile = useMediaQuery("(min-width:600px)")
   return(
     <Carousel
     infiniteLoop={true}
@@ -40,7 +40,7 @@ const MainCarousel = () => {
          <NavigateBeforeIcon sx={{fontSize: 40}} />
         </IconButton>
     )}
-    renderArrowNext={(onClickHandler, hasPrev, label) =>(
+    renderArrowNext={(onClickHandler, hasNext, label) =>(
         <IconButton
         onClick={onClickHandler}
         sx={{
@@ -58,8 +58,39 @@ const MainCarousel = () => {
     )}
     
     >
+   {Object.values(heroTextureImports).map((texture, index) =>(
+    <Box key={`carousel-image-${index}`}>
+       <img 
+        src={texture}
+        alt={`carousel-${index}`}
+        style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+        }}
+       />
+       <Box
+       color="white"
+       padding="20px"
+       borderRadius="1px"
+       textAlign="left"
+       backgroundColor="rgb(0, 0,0,0.4)"
+       position= "absolute"
+       top="46%"
+       left={isNonMobile ? "10%" : "0"}
+       right={isNonMobile ? undefined : "0"}
+       margin={isNonMobile ? undefined : "0 auto"}
+       maxWidth={isNonMobile ? undefined : "240px"}
 
-
+       >
+       <Typography color={shades.secondary[200]}> --New ITEMS</Typography>
+       <Typography variant="h1"> Summer Sale</Typography>
+       <Typography fontWeight="bold" color={shades.secondary[300]} sx={{textDecoration : "underline"}}>Discover More</Typography>
+       </Box>
+    </Box>
+   ))}
+ 
     </Carousel>
   )
 }
